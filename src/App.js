a3313f4dcb1e6b6a56f9b7b9a42c4cd7969e7b9c
marci papineau <marcidenmark@hotmail.com>
2018-04-12 15:54:07 +0200

    loading json data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Flight from './components/Flight';
 
+
+
 const URL = 'https://gist.githubusercontent.com/marcidenmark/b7d5160b19cf97b6d08c64dd68c5cc4c/raw/fbb0ed9382fb7238733db1ee38563ef4a8563014/flights.json';
 //never change the state directly!
 class App extends Component {
--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,20 +1,21 @@
 import React from "react";
+import "./flight.css"
 
 class Flight extends React.Component {
 	render() {
 		return (
 			<div>
-				<h2>
-					{this.props.Destination}
-				</h2>
-				<p>
-					{this.props.arivalTime}
+				<h4 className="flight-city">
+					Destination: <br/> {this.props.Destination}
+				</h4>
+				<p> Arival Time
+					<br/>{this.props.arivalTime}
 				</p>
-				<p>
-					{this.props.departureTime}
+				<p> Departure Time
+					<br/>{this.props.departureTime}
 				</p>
-				<p>
-					{this.props.flightNumber}
+				<p> Flight Number
+					<br/>{this.props.flightNumber}
 				</p>
 			</div>)
 	}
